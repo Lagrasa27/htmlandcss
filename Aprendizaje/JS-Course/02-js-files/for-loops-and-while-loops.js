87742d35b1daed065ef80e244223b7777e8c745b
lagrasa27 <jorgebrito2733@gmail.com>
2023-08-01 19:01:54 -0400

    JS course loops and while loops

---
+++ Aprendizaje/JS-Course/02-js-files/for-loops-and-while-loops.js
@@ -0,0 +1,47 @@
+// for loops
+
+// for (let i = 0; i < 5; i++){
+//     console.log('in loop:', i) // u can use this formula in poe to see how many of this value are let say in data base
+// }
+
+// console.log('loop finished');
+
+// const names = ['shaun', 'mario', 'luigi']  // same has here
+  // same has here
+
+// for (let i = 0; i < names.length; i++){
+    // console.log(names[i])
+// let html = `<div>${names[i]}</div>`
+//     console.log(html)
+//     }
+
+
+
+
+
+// while loops
+
+const names = ['shaun', 'mario', 'luigi'];
+
+// let i = 0;
+
+// while (i < 5){
+//     console.log('in loop:', i)
+//     i++
+// }
+
+// let i = 0;
+// while (i <names.length){
+//     console.log(names[i])
+//     i++;
+// }
+
+// do while loops
+
+let i = 2
+
+do {
+    console.log('val of i is :', i)
+    i++;
+}
+while(i <5);
