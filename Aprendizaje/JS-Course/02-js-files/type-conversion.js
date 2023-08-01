a5720122db3b3d3888e7dd8d2d2b299bdb4502d8
lagrasa27 <jorgebrito2733@gmail.com>
2023-08-01 17:28:48 -0400

    JS course

---
+++ Aprendizaje/JS-Course/02-js-files/type-conversion.js
@@ -0,0 +1,15 @@
+// type conversion
+
+let score = '100';
+
+// score = Number(score) // using this we can change type converstion we just changed string to a number
+// console.log(score +1);
+// console.log(typeof score)
+
+// let result = Number('hello');
+// let result = String(50); // typeof very importan to use
+// let result = Boolean(0); // cero consider as a false value in js
+let result = Boolean('@'); // any boolean strin is true but if there no value then is false
+
+
+console.log(result, typeof result)
