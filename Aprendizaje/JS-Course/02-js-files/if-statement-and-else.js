f0f4b7c6dcee2fbeabf321e66e83508d84747a84
lagrasa27 <jorgebrito2733@gmail.com>
2023-08-03 15:53:13 -0400

    JS course if statements

---
+++ Aprendizaje/JS-Course/02-js-files/if-statement-and-else.js
@@ -0,0 +1,27 @@
+// if statements
+// const age = 23;
+//
+// if(age > 20){
+//     console.log('you are over 20 years olds');
+//
+// }
+//
+// const wars = ['king', 'master', 'horse', 'knigth']
+//
+// if (wars.length > 4){
+//     console.log(" that's a lof of wards going on this year")
+// }
+//
+//
+
+
+const password = 'pasfadadf';
+
+if (password.length >= 12){
+    console.log('that passowrd is mighty strong');
+}else if (password.length >= 8){
+    console.log(' that password is long enought !!');
+} else {
+    console.log('password is not long enough')
+
+}
