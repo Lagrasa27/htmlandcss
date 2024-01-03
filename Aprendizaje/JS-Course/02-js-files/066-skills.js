5ce32c83adfa9d3c1c70e706f413e543a1e9f479
lagrasa27 <jorgebrito2733@gmail.com>
2024-01-03 18:01:59 -0400

    getting there

---
+++ Aprendizaje/JS-Course/02-js-files/066-skills.js
@@ -0,0 +1,30 @@
+
+const driver =  60
+
+switch (driver){
+    case 100 :
+        console.log( 'pass')
+        break
+     case 90 :
+        console.log( 'pass')
+        break
+     case 80 :
+        console.log( 'pass')
+        break
+     case 70 :
+        console.log( 'pass')
+        break
+     case 60 :
+        console.log( 'fail')
+        break
+     case 50 :
+        console.log( 'fail')
+        break
+    default:{
+        console.log(' not evaluated yet')
+    }
+
+}
+
+
+
