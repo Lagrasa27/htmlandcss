abb67298e42ae0379e76e6da5586aa33c5f5883d
Jorge Elias Brito de la Cruz <71567819+Lagrasa27@users.noreply.github.com>
2024-03-25 09:12:39 -0400

    wheather proyect completed.

--- Aprendizaje/JS-Course/02-js-files/068-testing.js
+++ Aprendizaje/JS-Course/02-js-files/068-testing.js
@@ -16,3 +16,21 @@ let joel = magic.concat(number);
 
 console.log(joel);
 // console.log(tittle);
+
+let año = 30;
+
+console.log(año != 31);
+
+
+let numero ='100';
+
+numero = Number(numero);
+console.log(numero + 1);
+console.log(typeof numero);
+
+
+const god = ['maria','juan', 'jose','miguel','ramon','pedro', 'juanito']
+
+for(let i = 0; i < god.length; i++){
+    console.log(god[i]);
+}
